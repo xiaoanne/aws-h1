e2e2334943a39ffb2b5b2f606744f085a286cadd
Anne wANG <huiyang.wang621@gmail.com>
2022-10-18 21:35:08 +1300

    update README.md

--- website_s3/state.tf
+++ website_s3/state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "anne-tfstate-store"
-    key            = "state/terraform.tfstate"
+    key            = "state-s3/terraform.tfstate"
     region         = "ap-southeast-2"
     encrypt        = true
     dynamodb_table = "terraform-state"
