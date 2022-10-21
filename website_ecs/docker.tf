1fda23d137c091f8ad2d95d164a89ba450fa3efb
Anne wANG <huiyang.wang621@gmail.com>
2022-10-21 17:09:37 +1300

    test docker build with github actions

--- website_ecs/docker.tf
+++ website_ecs/docker.tf
@@ -1,12 +1,14 @@
-resource "docker_image" "anne_test_website_docker" {
-  name = "anne-test-website-docker"
-  build {
-    path = "../website"
-    dockerfile = "Dockerfile"
-    tag  = ["anne-test-website:10_22"]
+#Not using this as Terraform doesn't pick up the changes made to Dockerfile
 
-    label = {
-      author : "Anne"
-    }
-  }
-}
+#resource "docker_image" "anne_test_website_docker" {
+#  name = "anne-test-website-docker"
+#  build {
+#    path = "../website"
+#    dockerfile = "Dockerfile"
+#    tag  = ["anne-test-website:10_22"]
+#
+#    label = {
+#      author : "Anne"
+#    }
+#  }
+#}
