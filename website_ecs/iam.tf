c9ea23215397353150f12774c0890f1d7107896f
Anne wANG <huiyang.wang621@gmail.com>
2022-10-21 17:14:12 +1300

    test docker build with github actions

--- website_ecs/iam.tf
+++ website_ecs/iam.tf
@@ -1,11 +1,11 @@
-resource "aws_iam_role" "anne_test_ecs_task_role" {
-  name               = "anne-test-ecs-task-iam-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
-
-  tags = local.tags
-}
-
-resource "aws_iam_role_policy_attachment" "anne_test_iam_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-  role       = aws_iam_role.anne_test_ecs_task_role.name
-}
+#resource "aws_iam_role" "anne_test_ecs_task_role" {
+#  name               = "anne-test-ecs-task-iam-role"
+#  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+#
+#  tags = local.tags
+#}
+#
+#resource "aws_iam_role_policy_attachment" "anne_test_iam_policy" {
+#  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+#  role       = aws_iam_role.anne_test_ecs_task_role.name
+#}
