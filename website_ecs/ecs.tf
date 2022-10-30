853232c79bae89975ed8fa5c2721f2490f0d935e
Anne wANG <huiyang.wang621@gmail.com>
2022-10-31 09:41:57 +1300

    try manual approval permissions

--- website_ecs/ecs.tf
+++ website_ecs/ecs.tf
@@ -19,12 +19,13 @@ resource "aws_ecs_task_definition" "anne_test_website_ecs_task" {
         }
       ],
       "memory": 512,
-      "cpu": 256
+      "cpu": 256,
+      "networkMode": "awsvpc"
     }
   ]
   DEFINITION
-  requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
-  network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
+  requires_compatibilities = ["EC2"] # Stating that we are using ECS Fargate
+#  network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
   memory                   = 512         # Specifying the memory our container requires
   cpu                      = 256         # Specifying the CPU our container requires
   execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
@@ -36,15 +37,15 @@ resource "aws_ecs_task_definition" "anne_test_website_ecs_task" {
 resource "aws_ecs_service" "anne-test-website-ecs-service" {
   name            = "anne-test-website-ecs-service"
   cluster         = aws_ecs_cluster.anne_test_website_ecs_cluster.id
-#  task_definition = aws_ecs_task_definition.anne_test_website_ecs_task.arn
-  task_definition      = "${aws_ecs_task_definition.anne_test_website_ecs_task.family}"
-  launch_type = "FARGATE"
-  desired_count   = 1
-#  iam_role = aws_iam_role.anne_test_ecs_task_role.arn
+  task_definition      = "${aws_ecs_task_definition.anne_test_website_ecs_task.arn}"
+  launch_type = "EC2"
+  scheduling_strategy  = "REPLICA"
+  desired_count        = 1
+  force_new_deployment = true
 
   network_configuration {
     subnets = local.private_subnets[*].id
-    assign_public_ip = false
+    assign_public_ip = true
     security_groups = [aws_security_group.load_balancer_security_group-1.id]
   }
 
