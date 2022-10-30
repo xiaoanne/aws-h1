a636aa12069fea0f33502db53f10afe70ea333ba
Anne wANG <huiyang.wang621@gmail.com>
2022-10-31 10:59:08 +1300

    try ecs

--- website_ecs/ecr.tf
+++ website_ecs/ecr.tf
@@ -1,10 +1,10 @@
-resource "aws_ecr_repository" "anne_test_website" {
-  name                 = "anne-test-website"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-
-  tags = local.tags
-}
+#resource "aws_ecr_repository" "anne_test_website" {
+#  name                 = "anne-test-website"
+#  image_tag_mutability = "MUTABLE"
+#
+#  image_scanning_configuration {
+#    scan_on_push = true
+#  }
+#
+#  tags = local.tags
+#}
